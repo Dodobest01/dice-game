19a3b47a9a12fadd8a672d49f1041f6492dc0cf4
DoDo <newwebsite332@gmail.com>
2020-05-05 01:35:41 +0800

    gg

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч,нэгдүгээр тоглогчийг 0,хоёрдугаар тоглогчийг 1 гэе
-var activePLayer = 1;
+var activePLayer = 0;
 
 //Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -18,9 +18,38 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+//
+//Шоог шидэх event
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  //1-6 dotorh neg toog avna
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //shoonii zurgiig web deer gargaj irne
   diceDom.style.display = "block";
+  // buusan toond hargalzah shoog gargah
   diceDom.src = "dice-" + diceNumber + ".png";
+  //buusan toon ni 1ees ylgaatai bol onoog nemne
+  if (diceNumber !== 1) {
+    // 1-ees ylgaatai too buuna
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePLayer).textContent = roundScore;
+  } else {
+    //ene toglogchiin eeljindee tsugluulsan onoog  0 bolgono.
+    document.getElementById("current-" + activePLayer).textContent = 0;
+    // herev idevhtai toglogch in 0 baival idevhtai toglogchiig 1 boldo
+    //ugui bol 0 bolgo
+    roundScore = 0;
+    activePLayer === 0 ? (activePLayer = 1) : (activePLayer = 0);
+
+    //ulaan tseg shiljuulne
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // dhoog tur alga bolgoh
+    diceDom.style.display = none;
+    // if (activePLayer === 0) {
+    //   activePLayer = 1;
+    // } else {
+    //   activePLayer = 0;
+    // }
+  }
 });
